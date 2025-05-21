c79deca1baef427a28f95754430e4b0f43a5c722
iskur <32363199+iskur@users.noreply.github.com>
2025-05-21 12:56:56 +0200

    chore: fix meteox2y_cy import
    Remove unused import statement

--- vg/meteo/__init__.py
+++ vg/meteo/__init__.py
@@ -1,5 +1,7 @@
+__all__ = ["meteox2y", "avrwind", "times", "meteox2y_cy"]
 
 from . import meteox2y
 from . import avrwind
 from .. import times
-from . import meteox2y_cy
+
+# from . import meteox2y_cy
