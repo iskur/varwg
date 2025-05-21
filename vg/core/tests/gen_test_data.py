ae791c91c78c3b3082e7e2cb5a4310262299de95
Dirk Schlabing <dirk.schlabing@iws.uni-stuttgart.de>
2025-05-21 14:35:36 +0200

    fix: Update data generation script
    - Updated the input file path to correctly reference `../sample.met`.
    - Added missing import for `config_konstanz` and commented out the
      configuration set.
    - Included additional parameters in the `VG` initialization: `infill`,
      `rain_method` ("regression" for now, might switch to "simulation"
      later), and adjusted the disaggregation call.
    -  Modified the output process to use `to_df` method with proper settings.

--- vg/core/tests/gen_test_data.py
+++ vg/core/tests/gen_test_data.py
@@ -1,21 +1,22 @@
-"""Generates data/test_out_sample.met using data/sample.met as input."""
+"""Generates data/test_out_sample.met using ../sample.met as input."""
 
 import os
 import shutil
 import tempfile
-import numpy as np
 import vg
 from vg.core.tests import test_vg
 
 config_template = vg.config_template
 vg.set_conf(config_template)
 
+# import config_konstanz as conf
+# vg.set_conf(conf)
+
 
 p, T = test_vg.p, test_vg.T
 
 
 def main():
-    vg.conf = config_template
     data_filepath = os.path.join(
         os.path.dirname(vg.core.__file__), "tests", "data"
     )
@@ -29,21 +30,28 @@ def main():
     # in order not to refit the present fit...
     cache_dir = tempfile.mkdtemp("vg_test_data_gen")
 
-    var_names = ("R", "theta", "Qsw", "ILWR", "rh", "u", "v")
     vg.reseed(test_vg.seed)
     met_vg = vg.VG(
-        var_names,
+        test_vg.var_names,
         met_file=test_in_data_filepath,
         cache_dir=cache_dir,
         data_dir=cache_dir,
         refit=True,
         verbose=True,
+        infill=True,
+        rain_method="regression",
     )
     met_vg.fit(p)
     vg.reseed(test_vg.seed)
     met_vg.simulate(T=T)
-    met_vg.disaggregate()
-    shutil.copy(met_vg.outfilepath, test_out_data_filepath)
+    met_vg.disaggregate(test_vg.disagg_varnames)
+    # shutil.copy(met_vg.outfilepath, test_out_data_filepath)
+    # met_vg.to_df("hourly output", with_conversions=False).to_csv(
+    #     test_out_data_filepath, sep="\t"
+    # )
+    met_vg.to_df("hourly output", with_conversions=True).to_csv(
+        test_out_data_filepath, sep="\t"
+    )
     shutil.rmtree(cache_dir)
 
 
