36d1221bb980a051a97a4776f2d7ee9b6d0b148b
iskur <32363199+iskur@users.noreply.github.com>
2025-05-22 10:44:55 +0200

    chore(setup.py): library_dirs extra_compile_args, version bump
    - Added `extra_compile_args` and `extra_link_args` with OpenMP flags
      for non-Windows platforms.
    - Bumped version from 1.3 to 1.4.
    - Added 'nose' to `setup_requires`.
    - Removed future install requirement

--- setup.py
+++ setup.py
@@ -19,30 +19,34 @@ except ImportError:
 
 if sys.platform == "win32":
     library_dirs = ["."]
+    extra_compile_args = []
     extra_link_args = []
 else:
     library_dirs = []
+    extra_compile_args = ["-fopenmp"]
     extra_link_args = ["-fopenmp"]
 
 extensions = [
     Extension(
-        "cresample",
+        "vg.time_series_analysis.cresample",
         ["vg/time_series_analysis/cresample" + ext],
         include_dirs=[np.get_include()],
         library_dirs=library_dirs,
     ),
     Extension(
-        "ctimes",
+        "vg.ctimes",
         ["vg/ctimes" + ext],
         include_dirs=[np.get_include()],
         library_dirs=library_dirs,
     ),
     Extension(
-        "meteox2y_cy",
+        "vg.meteo.meteox2y_cy",
         ["vg/meteo/meteox2y_cy" + ext],
         include_dirs=[np.get_include()],
-        extra_compile_args=["-fopenmp"],
-        extra_link_args=["-fopenmp"],
+        # extra_compile_args=['-fopenmp'],
+        # extra_link_args=['-fopenmp'],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         library_dirs=library_dirs,
     ),
 ]
@@ -56,14 +60,12 @@ else:
 
 setup(
     name="vg",
-    version="1.3",
+    version="1.4",
     packages=["vg", "vg.core", "vg.meteo", "vg.time_series_analysis"],
     cmdclass=cmdclass,
     ext_modules=ext_modules,
-    # ext_modules=cythonize(extensions),
-    # ext_modules=extensions,
     scripts=["distribute_setup.py"],
-    setup_requires=["numpy"],
+    setup_requires=["numpy", "nose"],
     python_requires=">=3.5",
     install_requires=[
         "matplotlib",
@@ -72,16 +74,12 @@ setup(
         "pandas",
         "cython",
         "tqdm",
-        "future",
         "timezonefinder",
         "PICOS",
         "cvxopt",
+        "ipyparallel",
+        "bottleneck",
     ],
-    # dependency_links=[
-    #     'http://sourceforge.net/projects/matplotlib/files/matplotlib/matplotlib-1.1.1/',
-    #     'http://sourceforge.net/projects/scipy/files/scipy/0.11.0b1/',
-    #     'http://sourceforge.net/projects/numpy/files/NumPy/1.6.2/',
-    #     ],
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         "": ["*.txt", "*.dat", "*.met", "*.rst", "*.pyx", "*.c"],
