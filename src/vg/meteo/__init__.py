cf400e09466140e4231ed130c4eb30368f812064
iskur <32363199+iskur@users.noreply.github.com>
2025-06-30 13:24:21 +0200

    refactor: Simplify __init__.py imports
    The `__init__.py` file in `src/vg/meteo` has been simplified by removing explicit imports.

--- src/vg/meteo/__init__.py
+++ src/vg/meteo/__init__.py
@@ -1,8 +1,1 @@
 __all__ = ["meteox2y", "avrwind", "times", "meteox2y_cy"]
-
-from . import meteox2y
-from . import meteox2y_cy
-from . import avrwind
-from .. import times
-
-# from . import meteox2y_cy
